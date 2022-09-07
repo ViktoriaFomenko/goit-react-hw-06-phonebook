fca5f36836c2ac1dae63e5b46415b99519a82c17
ViktoriaFomenko <aktavia@gmail.com>
2022-09-07 16:47:06 +0300

    no root reducer

--- src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import filterReducer from './filter/filter -reducer';
-import contactsReducer from './contacts/contacts -reducer';
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-export default rootReducer;
